1916e837d3b2250db52c7f4bbde5f54e33a412ec
Subwoofy <subwoofgaming@gmail.com>
2021-01-22 12:53:20 +0100

    Impemented skill swapping

--- Scripts/UI.gd
+++ Scripts/UI.gd
@@ -1,16 +1,42 @@
 extends CanvasLayer
 
+onready var ui = $UI
 onready var action_bar_path = "UI/ActionBarManaOverlay/HBoxContainer/"
-
+onready var player = self.get_tree().get_root().get_node("Game/NavigationMap/YSort/Player")
+onready var skill_tooltip = preload("res://Scenes/UI/SkillTooltip.tscn")
 
 var loaded_skills
 
 func _ready():
-	loaded_skills = self.get_tree().get_root().get_node("Game/NavigationMap/YSort/Player").action_bar_skills
 	load_shortcuts()
 
 func load_shortcuts():
+	loaded_skills = self.player.action_bar_skills
 	for shortcut in loaded_skills.keys():
 		var skill_icon = load("res://Sprites/UI/SkillIcons/" + loaded_skills[shortcut] + "_Icon.png")
 		self.get_node(action_bar_path + shortcut + "/SkillIcon").set_normal_texture(skill_icon)
 
+func show_skill_tooltip(skill_name):
+	if skill_name == "":
+		return
+	var tooltip = self.skill_tooltip.instance()
+	var skill_data = DataImport.skill_data[skill_name].duplicate()
+	var regex = RegEx.new()
+	regex.compile("\\$(?<name>([a-z]|[A-Z])*).(?<index>[0-9]+)")
+	var result = regex.search_all(skill_data["SkillTooltip"])
+	if skill_data["SkillDamage"][1] > 0:
+		skill_data["SkillDamage"] = self.player.damage(skill_data["SkillTags"], false, skill_data["SkillDamage"])["minmax"]
+		skill_data["SkillDamage"] = [round(skill_data["SkillDamage"][0]), round(skill_data["SkillDamage"][1])]
+	for i in len(result):
+		regex.compile("\\$" + result[i].get_string("name") + "." + result[i].get_string("index"))
+		skill_data["SkillTooltip"] = regex.sub(skill_data["SkillTooltip"], "[color=#9a94ee]" + str(skill_data[result[i].get_string("name")][int(result[i].get_string("index"))]) + "[/color]")
+
+	tooltip.description = skill_data["SkillTooltip"]
+	tooltip.mana_cost = skill_data["SkillManaCost"]
+	tooltip.use_time = skill_data["SkillCastTime"]
+	tooltip.tags = skill_data["SkillTags"]
+	regex.compile("_")
+	tooltip.ic = load("res://Sprites/UI/SkillIcons/" + skill_name + "_Icon.png")
+	tooltip.skill_name = " " + regex.sub(skill_name, " ")
+	self.ui.add_child(tooltip)
+	return tooltip
---
+++ Scripts/UI/SkillSelection.gd
@@ -0,0 +1,49 @@
+extends GridContainer
+
+onready var game = self.get_tree().get_root().get_node("Game")
+var selected_slot
+var selectable_skills = {}
+var current_tooltip = null
+
+func _ready():
+	pass # Replace with function body.
+
+func _on_Skill_gui_input(event, skill_slot):
+	if event is InputEventMouseButton and event.button_index == BUTTON_LEFT and event.pressed:
+		self.visible = !self.visible
+		self.selected_slot = skill_slot
+		print(self.game.player.learned_skills.keys())
+		for skill in self.game.player.learned_skills:
+			if self.selectable_skills.has(skill):
+				continue
+			var skill_icon = TextureRect.new()
+			skill_icon.texture = load("res://Sprites/UI/SkillIcons/" + skill + "_Icon.png")
+			skill_icon.connect("mouse_entered", self, "_on_Skill_mouse_entered", [skill])
+			skill_icon.connect("mouse_exited", self, "_on_Skill_mouse_exited")
+			skill_icon.connect("gui_input", self, "_on_Skill_click", [skill])
+			skill_icon.expand = true
+			skill_icon.rect_min_size = Vector2(50, 50)
+			self.selectable_skills[skill] = self.game.player.learned_skills[skill]
+			self.rect_size = Vector2(self.rect_size.x, 50)
+			self.add_child(skill_icon)
+
+
+func _on_Skill_mouse_entered(skill):
+	self.current_tooltip = self.game.ui.get_parent().show_skill_tooltip(skill)
+
+func _on_Skill_mouse_exited():
+	if self.current_tooltip == null:
+		return
+	self.current_tooltip.queue_free()
+	self.current_tooltip = null
+
+func _on_Skill_click(event, skill):
+	if event is InputEventMouseButton and event.button_index == BUTTON_LEFT and event.pressed:
+		self.game.player.action_bar_skills["Skill" + str(self.selected_slot + 1)] = skill
+		self.game.ui.get_parent().load_shortcuts()
+		self.game.player.update_selected_skills()
+		self.visible = false
+		if self.current_tooltip == null:
+			return
+		self.current_tooltip.queue_free()
+		self.current_tooltip = null
